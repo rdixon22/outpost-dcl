07be7f1d287527182fffaa7605b970b796aef089
rdixon <rob@terrific.us>
2019-01-31 20:49:53 -0800

    Small update, cleaning up an unused file and updating the Readme.

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ This project is the start of some adventure-game style features that didn't quit
 * And probably more...
 
 As usual, GLTF model conversions are weird, as you will plainly see when the rabbits mutate. 
+
+To experience it, just walk around. There's a cat, and some bunnies. There's a note you can find and click on. There's a mountain with something on it.
--- src/projector.ts
+++ src/projector.ts
@@ -1,4 +1,3 @@
-import { SpriteSheet } from "./spritesheet";
 import { SpriteShape } from "./spriteshape";
 
 @Component("Projector")
@@ -11,11 +10,7 @@ export class Projector {
 
   public idleAnim:AnimationClip;
   public attackAnim:AnimationClip;
-  public damageAnim:AnimationClip;
-
-  public attackFx:SpriteSheet;
-  public idleFx:SpriteSheet;
-  public damageFx:SpriteSheet;
+  public broadcastAnim:AnimationClip;
 
   constructor(_name:string, _modelFile:string, _position: Vector3, _scale: Vector3) {
     this.modelFile = _modelFile;
@@ -29,7 +24,7 @@ export class Projector {
 
     this.ent.set(
       new OnClick(() => {
-        this.attack();
+        this.broadcast();
       })
     )
   }
@@ -44,11 +39,11 @@ export class Projector {
 
     mod.set(new Transform({ scale: _scale})); // position: _position, 
 
-    this.attackAnim = new AnimationClip("MeleeAttack", {loop: false});
+    this.attackAnim = new AnimationClip("Attack", {loop: false});
     this.shape.addClip(this.attackAnim);
 
-    this.damageAnim = new AnimationClip("Damage", {loop: false});
-    this.shape.addClip(this.damageAnim);
+    this.broadcastAnim = new AnimationClip("Broadcast", {loop: false});
+    this.shape.addClip(this.broadcastAnim);
 
     this.idleAnim = new AnimationClip("Idle", {loop: true, speed: 0.5});
     this.shape.addClip(this.idleAnim);
@@ -64,70 +59,19 @@ export class Projector {
 
   }
 
-  attack() {
-    log("attackAnim.playing=" + this.attackAnim.playing + ", dirty=" + this.attackAnim.dirty);
-    //this.attackAnim = new AnimationClip("MeleeAttack", {loop: false});
-    //if (this.attackAnim.playing) return;
-    this.attackAnim.playing = false;
-    log("attackAnim.playing=" + this.attackAnim.playing + ", dirty=" + this.attackAnim.dirty);
-    this.attackAnim.play();
-    if (this.attackFx)
-    {
-      this.attackFx.entity.alive = true;
-      this.attackFx.animate();
-    }
-  }
-
-  damage() {
-    this.damageAnim.play();
-    if (this.damageFx)
-    {
-      this.damageFx.entity.alive = true;
-      this.damageFx.animate();
-    }
-  }
-
-  idle() {
-    this.idleAnim.play();
-    if (this.idleFx)
-    {
-      this.idleFx.animate(true);
-    }
-  }
-
-  addIdleFx(_pos:Vector3, _rot:Vector3, _fileName:string, _color:Color3, _framesX: number = 1, _framesY: number = 1, _staticFrame:number = 0, _w: number = 1, _h: number = 1)
+  broadcast()
   {
-    const spriteFx = this.createSpritePlane(_pos, _rot, _fileName, _color, _framesX, _framesY, _staticFrame, _w, _h);
-    spriteFx.alive = false;
-    this.idleFx = spriteFx.get(SpriteSheet);
-    //log("spritePlane.pos=" + JSON.stringify(spriteFx.get(Transform).position));
+
   }
 
-  addAttackFx(_pos: Vector3, _rot: Vector3, _fileName:string, _color:Color3, _framesX: number = 1, _framesY: number = 1, _staticFrame:number = 0, _w: number = 1, _h: number = 1)
+  attack()
   {
-    const spriteFx = this.createSpritePlane(_pos, _rot, _fileName, _color, _framesX, _framesY, _staticFrame, _w, _h);
-    spriteFx.alive = false;
-    this.attackFx = spriteFx.get(SpriteSheet);
+
   }
 
-  addDamageFx(_pos: Vector3, _rot: Vector3, _fileName:string, _color:Color3, _framesX: number = 1, _framesY: number = 1, _staticFrame:number = 0, _w: number = 1, _h: number = 1)
+  idle()
   {
-    const spriteFx = this.createSpritePlane(_pos, _rot, _fileName, _color, _framesX, _framesY, _staticFrame, _w, _h);
-    spriteFx.alive = false;
-    this.damageFx = spriteFx.get(SpriteSheet);
+
   }
 
-  createSpritePlane(_pos: Vector3, _rot: Vector3, _fileName:string, _color:Color3, _framesX: number = 1, _framesY: number = 1, _staticFrame:number = 0, _w: number = 1, _h: number = 1)
-  {
-    const spritePlane = new Entity();
-    var sheet = new SpriteSheet(spritePlane, _fileName, _color,  _framesX, _framesY, _staticFrame, _w, _h);
-    spritePlane.set(sheet);
-    spritePlane.setParent(this.ent);
-
-    spritePlane.set(new Transform({ position: _pos }));
-    spritePlane.get(Transform).rotation.eulerAngles = _rot;
-    
-    engine.addEntity(spritePlane); 
-    return spritePlane;
-  }
 }
